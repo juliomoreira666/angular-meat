c029bfd972db12b76a6c6b04a79a0dae80af7698
juliomoreira666 <julio@codeby.com.br>
2018-03-15 01:12:35 -0300

    Routes and urls

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { Routes } from "@angular/router";
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
+]
