947d8353d454ef7537cb8d36d4f5523f3c301e96
juliomoreira666 <julio@codeby.com.br>
2018-03-15 01:09:34 -0300

    Routes and urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
+import { ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
     HttpModule,
     RouterModule
   ],
